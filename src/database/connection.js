b841fc8912fa2174eda7a5c2c4f864169c6c0a5e
fernndes <you@example.com>
2021-01-26 19:13:22 -0300

    Added Procfile

--- src/database/connection.js
+++ src/database/connection.js
@@ -7,7 +7,7 @@ const pool = new pg.Pool({
     database: process.env.DB_DATA,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
-     ssl: {
+    ssl: {
         rejectUnauthorized: false
       }
 });
--- src/database/migrations/create_users.js
+++ src/database/migrations/create_users.js
@@ -1,4 +1,4 @@
-const db = require("./config")
+const db = require("../connection")
 
 async function createTable() {
     await db.connect()
