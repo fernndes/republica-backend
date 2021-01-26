986363e0acd79d6c4779c57863d148aec782c037
fernndes <you@example.com>
2021-01-26 20:45:34 -0300

    Errors fixed

--- src/database/connection.js
+++ src/database/connection.js
@@ -6,7 +6,10 @@ const client = new pg.Pool({
     host: process.env.DB_HOST,
     database: process.env.DB_DATA,
     password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false
+      }
 });
 
 module.exports = client;
