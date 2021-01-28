fe3f9160079628f590ef9881c25b2aaba7637675
fernndes <you@example.com>
2021-01-27 21:58:27 -0300

    Fixing errors

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { celebrate, Segments, Joi } = require('celebrate');
 
-const UserController = require('./controllers/UserController');
+const UserController = require('./src/controllers/UserController');
 
 const routes = express.Router();
 
