50fe5aff166c7a27238836a12271e5f65707cfc5
fernndes <you@example.com>
2021-01-27 21:54:15 -0300

    Fixing errors

--- routes.js
+++ routes.js
@@ -6,7 +6,7 @@ const UserController = require('./controllers/UserController');
 const routes = express.Router();
 
 routes.post('/users', celebrate({
-	[Segments.BODY] = Joi.object().keys({
+	[Segments.BODY] : Joi.object().keys({
 		key: Joi.string().required().min(8),
 		name: Joi.string().required(),
         email: Joi.string().required().email(),
