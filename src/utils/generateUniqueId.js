867e4564c8338de8270bb66b92ece1ed29a3e338
fernndes <you@example.com>
2021-01-27 21:32:44 -0300

    Fixing errors

--- src/utils/generateUniqueId.js
+++ src/utils/generateUniqueId.js
@@ -3,6 +3,7 @@ var bcrypt = require('bcryptjs');
 function generateUniqueId(key) {
 	var salt = bcrypt.genSaltSync(10);
 	var hash = bcrypt.hashSync(`${key}`, salt);
+	console.log(hash)
 	return hash
 }
 
