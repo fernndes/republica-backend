1f5d6abbb79b3b61d8c450435d3b6e714750a3e4
fernndes <you@example.com>
2021-02-01 13:59:15 -0300

    Improving key generation

--- src/utils/generateUniqueId.js
+++ src/utils/generateUniqueId.js
@@ -1,7 +1,7 @@
 var bcrypt = require('bcryptjs');
 
 function generateUniqueId() {
-	var key = Math.random().toString(36).slice(-10);
+	var key = Math.random().toString(36).slice(2);
 	var salt = bcrypt.genSaltSync(10);
 	var hash = bcrypt.hashSync(`${key}`, salt);
 	return key
